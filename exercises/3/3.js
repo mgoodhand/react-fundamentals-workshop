338061aa4fa0e7dbc363785eb2185e6a418d9d01
Mark Goodhand <mrg@corefiling.com>
2018-03-22 10:23:33 +0000

    Additional prop and a default.

--- exercises/3/3.js
+++ exercises/3/3.js
@@ -6,13 +6,13 @@ import React from 'react'
 const HelloWorld = props => {
   // TODO: pass through another prop to customise the greeting
   // rather than it always be hardcoded as Hello
-  return <h1>Hello, {props.name}</h1>
+  return <h1>{props.greeting || 'Hello'}, {props.name}</h1>
 }
 
 ReactDOM.render(
   // props are all just JavaScript, so you can pass through any data type - these aren't HTML attributes
   <div>
-      <HelloWorld name="Mark" />
+      <HelloWorld name="Mark" greeting="Hi"/>
       <HelloWorld name="Bob" />
   </div>,
   document.getElementById('react-root')
